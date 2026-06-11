8e12291035bd715db32b277c1222581142b94c50
Tornike Barbakadze <tbar3242@gmail.com>
2026-06-11 12:47:11 +0400

    ჯავასკრიპტის პირველი დავალება

---
+++ Javascript/1/script.js
@@ -0,0 +1,12 @@
+// 1. ორი ცვლადი რიცხვ
+let number1 = 15;
+let number2 = 25;
+
+// 2. არითმეტიკული მოქმედება
+let totalSum = number1 + number2;
+
+// 3. შედეგი კონსოლში (F12 ინსპექტორისთვის)
+console.log("ორი რიცხვის ჯამი არის:", totalSum);
+
+// 4. შედეგის გამოვტანა HTML დოკუმენტში ID-ის მიხედვით
+document.getElementById("result-box").innerText = totalSum;
